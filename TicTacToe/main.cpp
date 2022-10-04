992d9eafc03475501c4209db6dbae18cba267c7f
Maikel Mirzadegan <91105959+Maikel-Mirza@users.noreply.github.com>
2022-10-04 00:31:53 +0000

    Update main.cpp

--- TicTacToe/main.cpp
+++ TicTacToe/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 // this is a tic-tac-toe game
 // you can have 2 or more players
-// a board will be generated and you have to pick a position to place your simble
+// a board will be generated and you have to pick a position to place your symbol
 // first player to get 3 or more squares to line up wins
 // ask the player for there name
 // let the player decide number of players
