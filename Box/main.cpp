7edf3cc95ce8ed1fd74a5c8f808724486130142b
Maikel Mirzadegan <91105959+Maikel-Mirza@users.noreply.github.com>
2023-01-09 18:24:06 -0500

    Add files via upload

---
+++ Box/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "Rectangle.h"
+
+
+
+int main(void){
+	Rectangle::help();
+	Rectangle recOne(10, 8, '|');
+	Rectangle recTwo = Rectangle();
+	
+
+	recOne.draw();
+	recOne.grow();
+	recOne.draw();
+	recTwo.draw();
+	recTwo.shrink();
+	recTwo.draw();
+	return 0;
+}
