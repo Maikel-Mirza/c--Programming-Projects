075217342a044394c6b48b79e2c06f1523f42c9c
Maikel Mirzadegan <mmirzadegan@myseneca.ca>
2023-01-10 15:53:45 -0500

    Adding a new project

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "Box.h"
+
+
+
+int main(void){
+	Box bx = Box(10, 10, 10, '*');
+	bx.draw();
+	bx.grow();
+	bx.draw();
+	bx.shrink();
+	bx.shrink();
+	bx.draw();
+	return 0;
+}
